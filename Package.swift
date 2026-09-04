43b1e5f7036fdfec5d6b1c77c4f7833eeb207a1f
Jeff Ching <chingor@google.com>
2026-09-04 15:41:48 -0700

    deps: use remote swift-google-rpc repo (#695)
    Towards #450

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
     .package(path: "../../packages/swift-google-gax"),
     .package(url: "https://github.com/googleapis/swift-google-wkt", from: "0.1.0-preview"),
     .package(path: "../../generated/swift-google-longrunning"),
-    .package(path: "../../generated/swift-google-rpc"),
+    .package(url: "https://github.com/googleapis/swift-google-rpc", from: "0.1.0-preview"),
     .package(path: "../../generated/swift-google-type"),
     .package(url: "https://github.com/apple/swift-log", from: "1.12.0"),
   ],
